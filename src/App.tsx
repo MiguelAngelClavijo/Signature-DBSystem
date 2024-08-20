2254427194f2371359ed0152f59586d3f8223a91
Miguel Angel Clavijo Sanchez <clavijom2000@gmail.com>
2024-08-20 17:10:16 -0500

    [Styles] Create styles React Email

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,17 @@ import Preview from "./Preview";
 import { typeForm } from "./types/formTypes";
 
 function App() {
+
   const [valorForm, setValorForm] = useState<typeForm>({
     nombre: "Miguel Angel Clavijo Sanchez",
     cargo: "Disenador Grafico",
     celular: 3052670670,
     telefono: 0,
     ext: 126,
-  });
+  }
+);
+
+  const [checkValue, setCheckValue] = useState<boolean>(true);
   return (
     <div className="flex flex-col gap-5 py-8 lg:gap-10">
       <Header />
